f898f28195008476f0daf05e718e137de0253522
Marijn Suijten <marijns95@gmail.com>
2020-08-12 01:06:43 +0200

    linaro_gcc: Always use full make command when generating defconfig
    The generated defconfig depends on the selected (cross) compiler(s).
    Make sure the compiler is supplied in addition to all other options, to
    generate an appropriate defconfig.

--- compile_linaro_gcc.sh
+++ compile_linaro_gcc.sh
@@ -9,12 +9,14 @@ _defconfig=aosp_${_platform}_${_device}_defconfig
 echo "==> Entering $_kernel_path"
 pushd "$_kernel_path" || (echo "ERROR: Failed to cd into kernel source!"; exit 1)
 
+_build_cmd="make $_make_vars CROSS_COMPILE=\"$_cross_compile\" CROSS_COMPILE_ARM32=\"$_cross_compile_32\""
+
 echo "==> Building $_defconfig"
-make $_make_vars $_defconfig
+$_build_cmd $_defconfig
 
 echo "==> Building $_targets with Linaro gcc"
-echo $_make_vars CROSS_COMPILE="$_cross_compile" $_targets
-time make $_make_vars CROSS_COMPILE="$_cross_compile" CROSS_COMPILE_ARM32="$_cross_compile_32" $_targets
+echo $_build_cmd $_targets
+time $_build_cmd $_targets
 
 echo "==> $_targets compiled successfully"
 popd
