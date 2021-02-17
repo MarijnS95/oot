8c2c2b2cf222ae11ed1318767cf8b64301c81893
Marijn Suijten <marijns95@gmail.com>
2021-02-17 01:17:15 +0100

    setup: Move ARM32 cross-compiler setup to compiler-specific files
    We'll want to pick a different compiler for the 32-bit vDSO in the near
    future.

--- setup_linaro_gcc.sh
+++ setup_linaro_gcc.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/bash
 
-_cross_compile="/data/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-"
 _cross_compile="/data/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-"
+# TODO: Cannot use gcc 4.9 since 4.14.218: https://github.com/sonyxperiadev/kernel/blame/409132320fe5e08d558e979ce92f39208e885010/include/linux/compiler-gcc.h#L155-L160
+_cross_compile_32="$ANDROID_ROOT/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-"
 
-_make_args+=" CROSS_COMPILE=$_cross_compile"
+_make_args+=" CROSS_COMPILE=$_cross_compile CROSS_COMPILE_ARM32=$_cross_compile_32"
