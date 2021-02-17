9d24575dc5be18de640f68c2596f73e2a14c3afe
Marijn Suijten <marijns95@gmail.com>
2021-02-17 01:17:15 +0100

    setup: Move ARM32 cross-compiler setup to compiler-specific files
    We'll want to pick a different compiler for the 32-bit vDSO in the near
    future.

--- setup_clang.sh
+++ setup_clang.sh
@@ -5,4 +5,7 @@ _cross_compile="$ANDROID_ROOT/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-andr
 _clang_version=$(awk '/^\* \[\*\*Android Linux Kernel/{f=NR} /^  \* Currently clang-/ && f==NR-1 {print $NF; exit}' "$ANDROID_ROOT/prebuilts/clang/host/linux-x86/README.md")
 _clang_path="$ANDROID_ROOT/prebuilts/clang/host/linux-x86/$_clang_version/bin"
 
-_make_args+=" CROSS_COMPILE=$_cross_compile CC=${_clang_path}/clang CLANG_TRIPLE=aarch64-linux-gnu"
+# TODO: Cannot use gcc 4.9 since 4.14.218: https://github.com/sonyxperiadev/kernel/blame/409132320fe5e08d558e979ce92f39208e885010/include/linux/compiler-gcc.h#L155-L160
+_cross_compile_32="$ANDROID_ROOT/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-"
+
+_make_args+=" CROSS_COMPILE=$_cross_compile CC=${_clang_path}/clang CLANG_TRIPLE=aarch64-linux-gnu CROSS_COMPILE_ARM32=$_cross_compile_32"
