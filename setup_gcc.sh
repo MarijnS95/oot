3ea626b6882cf9ea5337ebd7aa9daa8b162eb2e8
Marijn Suijten <marijns95@gmail.com>
2021-02-17 01:17:52 +0100

    setup: Use system-wide aarch64-linux-gnu-gcc and arm-none-eabi-gcc
    Migrate deprecated AOSP gcc to a separate compiler target, `aosp_gcc`,
    while using the `gcc` target for a more recent and more relevant
    compiler that might be installed on ones system. The script can easily
    be called with `PATH=/path/to/your/arm/gcc:/and/aarch64 ./oot.sh` as
    well.

--- setup_gcc.sh
+++ setup_gcc.sh
@@ -1,7 +1,5 @@
 #! /usr/bin/bash
 
-_cross_compile="$ANDROID_ROOT/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
-# TODO: Cannot use gcc 4.9 since 4.14.218: https://github.com/sonyxperiadev/kernel/blame/409132320fe5e08d558e979ce92f39208e885010/include/linux/compiler-gcc.h#L155-L160
-_cross_compile_32="$ANDROID_ROOT/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-"
+# Use gcc available on path. On archlinux, `pacman -S aarch64-linux-gnu-gcc arm-none-eabi-gcc`
 
-_make_args+=" CROSS_COMPILE=$_cross_compile CROSS_COMPILE_ARM32=$_cross_compile_32"
+_make_args+=" CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-none-eabi-"
